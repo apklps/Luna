617a68fb4f7d3401579c242ea81cc6a025b7e822
apklps <apklpsltd@gmail.com>
2020-01-18 16:10:24 -0700

    renamed some functions, added more story

--- Luna.py
+++ Luna.py
@@ -1,14 +1,14 @@
 import os, time
 
-def open_file(game_state):
+def openFile(gameState):
 
 	# Opens option file depending on the player's game state
 
-	name = 'options_' + str(game_state) + '.txt'
+	name = 'options_' + str(gameState) + '.txt'
 	file = open(name, 'r')
 	return file
 
-def print_options(file):
+def printOptions(file):
 
 	# Prints out the options in the text file
 
@@ -18,6 +18,14 @@ def print_options(file):
 		print(str(i) + line, end = '')
 		i = i + 1
 	print('\n')
+
+def getPlayerInput(gameState):
+
+	print('What do you do?\n')
+	file = openFile(gameState)
+	printOptions(file)
+	answer = input('Option: ')
+	return answer
 
 def main():
 
@@ -36,43 +44,62 @@ def main():
 	# Defining needed variables
 
 	laptop = False # Player doesn't have the laptop
-	injury = False # Player doesn't have an injury
-	game_state = 1 # Saves the progression of the game
+	injuryUpper = False # Player doesn't have an upper-body injury
+	injuryLower = False # Player doesn't have an upper-body injury
+	gameState = 1 # Saves the progression of the game
+	gameEnd = False
 
 	# Beginning the game
 
 	os.system('cls||clear')
 
-	# Game State One (No laptop, first room)
+	while injuryUpper == False or injuryLower == False:
 
-	if answer == 'Y':
-		print('You wake up in a dimly lit room.')
-		print('You see a door on the opposite wall of you.')
-		print('You see a laptop on a shelf on the left wall.')
-		print('You see a window on the right wall.\n')
+		# Game State One (No laptop, first room)
 
-		# Loops options until the player grabs the laptop
+		if answer == 'Y':
+			print('You wake up in a dimly lit room.')
+			print('You see a door on the opposite wall of you.')
+			print('You see a laptop on a shelf on the left wall.')
+			print('You see a window on the right wall.\n')
 
-		while laptop == False:
+			# Loops options until the player grabs the laptop
 
-			print('What do you do?\n')
-			file = open_file(game_state)
-			print_options(file)
+			while laptop == False:
 
-			answer = input('Option: ')
+				answer = getPlayerInput(gameState)
 
-			if answer == '1':
-				os.system('cls||clear')
-				print('The laptop is unlocked. It has Kali Linux installed on it.\n')
-				laptop = True # The player now has the laptop
-				game_state = game_state + 1 # The game_state is now at 2
+				if answer == '1': # Player grabs the laptop
+					os.system('cls||clear')
+					print('The laptop is unlocked. It has Kali Linux installed on it.\n')
+					laptop = True # The player now has the laptop
+					gameState = gameState + 1 # The gameState is now at 2
 
-			elif answer == '2':
-				os.system('cls||clear')
-				print('The door has an electronic numpad. You don\'t know the code.\n')
+					# Start of gameState two (escaping the room)
 
-			elif answer == '3':
-				os.system('cls||clear')
-				print('You can see outside. There is a security sensor on the edge of the window.\n')
+					answer = getPlayerInput(gameState)
+
+					if answer == '1': # Player tries to opens the door
+						os.system('cls||clear')
+						print('The door has an electronic numpad. You don\'t know the code.\n')
+						gameState = gameState + 98 # gameState is at 100
+						answer = getPlayerInput(gameState)
+
+						if answer == '1': # Hack the door
+							os.system('cls||clear')
+							print('You successfully hacked the door!')
+
+
+					elif answer == '1': # Player tries to open the window
+						pass
+						gameState = gameState + 1 # gameState is at 3
+
+				elif answer == '2': # Player tries to open the door
+					os.system('cls||clear')
+					print('The door has an electronic numpad. You don\'t know the code.\n')
+
+				elif answer == '3': # Player tries to open the window
+					os.system('cls||clear')
+					print('You can see outside. There is a security sensor on the edge of the window.\n')
 
 main()
