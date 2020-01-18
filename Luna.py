60294d07425911f5e40f1fc9930bafd9e8a3a685
apklps <apklpsltd@gmail.com>
2020-01-18 14:23:42 -0700

    complete brokenness

--- Luna.py
+++ Luna.py
@@ -1,5 +1,15 @@
 import os, time
 
+def open_file():
+
+	name = 'options_' + str(game_state)
+	print(name)
+	return file = open(name, 'r')
+
+def close_file():
+
+	file.close()
+
 def print_options(options):
 
 	i = 1
@@ -8,6 +18,10 @@ def print_options(options):
 		print(i, line, end = '')
 		i = i + 1
 	print('\n')
+
+def remove_options(option):
+
+	
 
 def main():
 
@@ -26,6 +40,7 @@ def main():
 	# Defining needed variables
 
 	laptop = False # Player doensn't have the laptop
+	game_state = 0 # Saves the progression of the game
 
 	# Beginning the game
 
@@ -38,7 +53,7 @@ def main():
 		print('You see a window on the right wall.\n')
 
 		print('What do you do?\n')
-		options_one = open('options_1.txt', 'r')
+		open_file()
 		print_options(options_one)
 
 		answer = input('Option: ')
@@ -47,13 +62,12 @@ def main():
 			os.system('cls||clear')
 			print('The laptop is unlocked. It has Kali Linux installed on it.\n')
 			laptop = True # The player now has the laptop
+			game_state = game_state + 1 # The game_state is now at 1
 
 		elif answer == '2':
 			os.system('cls||clear')
 			print('The door has an electronic numpad. You don\'t know the code.\n')
-			print('What do you do?')
-			print('1) Grab the laptop.')
-			print('2) Try to open the window.')
+			remove_options(answer)
 		elif answer == '3':
 			os.system('cls||clear')
 			print('You can see outside. There is a security sensor on the edge of the window.\n')
