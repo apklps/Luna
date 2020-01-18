7089d7591976c14122a1c6be7100101fe6c16a51
apklps <apklpsltd@gmail.com>
2020-01-18 12:21:20 -0700

    Update Luna.py

--- Luna.py
+++ Luna.py
@@ -1,5 +1,9 @@
 def main():
 
-	print('Hello, I\'m Luna.')
+	print('HackED 2020 - Luna\n')
+	print('Developed in a 24-hour timeframe by Dylan and Taylor.\n')
+	print('Luna is a program developed to help individuals reach')
+	print('self-actualization through a series of questions.\n')
+	input('Would you like to proceed? (Y/N) ')
 
 main()
