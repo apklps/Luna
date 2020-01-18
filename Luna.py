3f55dd63d4443fa28064d262d62643b8c19ca09a
tcurran13 <tcurran@ualberta.ca>
2020-01-18 12:25:50 -0700

    dictionary addition
    add

--- Luna.py
+++ Luna.py
@@ -5,6 +5,10 @@ def main():
 	print('Luna is a program developed to help individuals reach')
 	print('self-actualization through a series of questions.\n')
 	answer = input('Would you like to proceed? (Y/N) ')
+	emotions = {'good': 'Thats boring'}
+
+
+
 
 	if answer = 'Y':
 		while talking:
