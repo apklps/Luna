198604f4a181ce371156dd537463e732789c51be
apklps <apklpsltd@gmail.com>
2020-01-18 13:45:55 -0700

    first option set

--- Luna.py
+++ Luna.py
@@ -11,34 +11,40 @@ def main():
 	print('HackED 2020 - Luna\n')
 	print('Developed in a 24-hour timeframe by Dylan and Taylor.\n')
 	print('Luna is a program developed to help individuals reach')
-	print('self-actualization through a series of questions.\n')
+	print('self-actualization through a series of events.\n')
 	answer = input('Would you like to proceed? (Y/N) ')
 
 	# Defining needed variables
 
-	talking = True
+	playing = True
 
-	# Beginning the conversation
+	# Beginning the game
 
 	os.system('cls||clear')
 
 	if answer == 'Y':
-		print('If you would like to quit at any time, just type "quit".\n')
-		print('The conversation will start in five seconds.')
-		time.sleep(5)
-		os.system('cls||clear')
+		print('You wake up in a dimly lit room.')
+		print('You see a door on the opposite wall of you.')
+		print('You see a laptop on a shelf on the left wall.')
+		print('You see a window on the right wall.\n')
 
-		# Initial Conversation
+		print('What do you do?')
+		print('1) Open the laptop.')
+		print('2) Look out the window.')
+		print('3) Try to open the door.\n')
 
-		print('Hello, my name is Luna.')
-		name = input('What\'s your name?\n\n' + 'My name is: ')
+		answer = input('Option: ')
 
-		print('\nHello %s' % name)
-
-		while talking:
-			pass
-			
-			
+		if answer == '1':
+			os.system('cls||clear')
+			print('\nThe laptop is unlocked. It has Kali Linux installed on it.\n')
+			print('What do you do?')
+			print('1) Try to open the window.')
+			print('2) Try to open the door.')
+		elif answer == '2':
+			print('\nYou can see outside. There is a security sensor on the edge of the window.')
+		elif answer == '3':
+			print('\nThe door is locked.')
 
 
 main()
