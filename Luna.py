a2df79766bee92e1897b94f3865eb481b5f3491e
apklps <apklpsltd@gmail.com>
2020-01-18 12:23:00 -0700

    initial setup

--- Luna.py
+++ Luna.py
@@ -4,6 +4,10 @@ def main():
 	print('Developed in a 24-hour timeframe by Dylan and Taylor.\n')
 	print('Luna is a program developed to help individuals reach')
 	print('self-actualization through a series of questions.\n')
-	input('Would you like to proceed? (Y/N) ')
+	answer = input('Would you like to proceed? (Y/N) ')
+
+	if answer = 'Y':
+		while talking:
+			pass
 
 main()
